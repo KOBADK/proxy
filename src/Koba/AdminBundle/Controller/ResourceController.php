4f24262c7a07843c73e1d02c70eeee95c3c5fbe8
Troels Ugilt Jensen <tujensen@gmail.com>
2015-03-19 11:28:30 +0100

    Issue #KOBA-105 by tuj: Added /admin/bookings controller. Removed unused files.

---
+++ src/Koba/AdminBundle/Controller/ResourceController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file
+ * Contains ResourceController.
+ */
+
+namespace Koba\AdminBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\RestBundle\Controller\Annotations as FOSRest;
+
+/**
+ * @Route("/resources")
+ */
+class ResourceController extends FOSRestController {
+  /**
+   * Get resources.
+   *
+   * @FOSRest\Get("")
+   *
+   * @return array
+   */
+  public function getResources() {
+    return array();
+  }
+}
