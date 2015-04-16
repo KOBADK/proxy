004249695527c2de90af5b80890b3b7696dc7fe5
Jesper Kristensen <cableman@linuxdev.dk>
2015-04-16 15:02:42 +0200

    Issue #KOBA-213 by cableman: Added enrichment of booking objects - part IV

--- src/Itk/ExchangeBundle/Services/ExchangeWebService.php
+++ src/Itk/ExchangeBundle/Services/ExchangeWebService.php
@@ -25,26 +25,6 @@ class ExchangeWebService {
   }
 
   /**
-   * Get list of rooms (resources) from Exchange.
-   */
-  public function getRessources() {
-    // Get list of rooms from Exchange. Only rooms that the Exchange
-    // administrators have allow you to get.
-    // @see https://social.msdn.microsoft.com/Forums/office/en-US/4ff04c60-48c2-4a69-ab75-2383e73bfde2/e2010ewsxmljavahow-to-list-all-resource-mailboxes-meeting-room-in-exchange-web-service-2010?forum=exchangesvrdevelopment
-    try {
-      $xml = $this->client->request('GetRoomLists', '<m:GetRoomLists/>');
-    }
-    catch (ExchangeSoapException $exception) {
-      // @TODO: do some error handling.
-    }
-
-    $dom = new \DOMDocument();
-    $dom->loadXML($xml);
-
-    // @TODO: Get the rooms and create resource entities?
-  }
-
-  /**
    * Get detailed information about a booking.
    *
    * @param $id
@@ -163,7 +143,7 @@ class ExchangeWebService {
 
     $body = $xpath->evaluate('./t:TextBody', $calendarItem);
     if ($body->length) {
-      $booking->setBody($body->item(0)->nodeValue);
+      $booking->setBody((string) $body->item(0)->nodeValue);
     }
 
     return $booking;
